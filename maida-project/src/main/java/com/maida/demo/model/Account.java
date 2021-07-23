d8eb4a4d975ae5af5c91f51fd27a12c2f7691392
Fellipe Gurgel Serra de Alencar <fellipegurgel93@gmail.com>
2021-07-23 14:10:27 -0300

    First commit - Create project, User and Security Config

---
+++ maida-project/src/main/java/com/maida/demo/model/Account.java
@@ -0,0 +1,46 @@
+package com.maida.demo.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account")
+public class Account {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer number;
+	
+	private BigDecimal balance;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+}
